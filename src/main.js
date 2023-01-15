74f29efb5e4b62cbbba7670a713bf0a4edd99a09
Raygud <runigudmundarson@hotmail.com>
2023-01-15 02:37:11 +0100

    Login page under construction

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Home from './pages/HomePage.vue'
 import ProductsPage from './pages/ProductsPage.vue'
 import ProductPage from './pages/ProductPage.vue'
 import ContactPage from './pages/ContactPage.vue'
+import LoginPage from './pages/LoginPage.vue'
 
 const history = createWebHistory()
 const router = createRouter({
@@ -13,10 +14,21 @@ const router = createRouter({
         { path: '/', component: Home },
         { path: '/Produkter', component: ProductsPage },
         { path: '/Produkt/:id', component: ProductPage, props: true, name: 'Produkt' },
-        { path: '/Kontakt', component: ContactPage }
-
-    ]
+        { path: '/Kontakt', component: ContactPage, beforeEnter: guard },
+        { path: '/Login', component: LoginPage }
+    ],
+    
+    
 })
+function guard(to, from, next) {
+    if (localStorage.getItem("token") !== null) {
+        next()
+    } else {
+        next('/Login')
+    }
+}
+
+
 
 createApp(App)
     .use(router)
